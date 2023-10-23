dd9b7e4448e38c999eb5b5611580eb0f752c04c7
jlopez1104 <145376782+jlopez1104@users.noreply.github.com>
2023-10-23 16:42:10 +0000

    done

--- conatiners2.py
+++ conatiners2.py
@@ -1,7 +1,10 @@
-musicians = ['Drake','Brent', 'travis','bryson']
-
-ask = input('Enter a artist: ')
-if ask in musicians:
+musicians = []
+for i in range(4):
+    name = input('Enter artist: ')
+    musicians.append(name)
+print(musicians)
+name2 = input('name person to see if they are on list: ')
+if name2 in musicians:
     print('this fruit is in the list')
 else:
     print ('fruit is not in the list')
